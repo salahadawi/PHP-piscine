5c2ee1695fb1be36c24d79e844a856f54377dc2e
Salah Adawi <sadawi@student.hive.fi>
2020-03-11 16:59:09 +0200

    finish ex02

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -4,15 +4,16 @@
 if ($argc == 2 && file_exists($argv[1]))
 {
 	$file = file_get_contents($argv[1]);
-	echo $file;
 	preg_match_all("/(?<=<a)[\s\S]*?(?=\/a>)/", $file, $matches);
-	print_r($matches);
 	$str = implode(" ", $matches[0]);
-	print($str);
 	preg_match_all("/(\"[\s\S]*?\"|>.+?<)/", $str, $matches);
-	print_r($matches);
+	$replacement = $matches[0];
+	foreach ($replacement as &$line)
+		$line = strtoupper($line);
+	foreach ($matches[0] as &$line)
+		$line = "/".$line."/";
+	$file = preg_replace($matches[0], $replacement, $file);
+	print($file);
 }
 
 ?>
-
-/((?<=>).*?(?=<)| (?<=\").*?(?=\"))/
