36b9348ed2fbf046805d38cb2a67e7fe3dcb509a
sadawi <hahli144@hotmail.com>
2020-06-23 17:17:58 +0300

    add average_user and moulinette_variance options

--- d01/ex13/agent_stats.php
+++ d01/ex13/agent_stats.php
@@ -30,9 +30,101 @@ if ($argc == 2)
 	}
 	if ($argv[1] == "average_user")
 	{
-
+		foreach($arr as $value)
+		{
+			if (!(strpos($value, "moulinette")))
+			{
+				$i = 0;
+				while ($value[$i] != ';')
+					$i++;
+				$i++;
+				while ($value[$i] != ';')
+					$i++;
+				$i++;
+				if (is_numeric($value[strpos($value, ';') + 1]))
+				{
+					$name = substr($value, 0, strpos($value, ';'));
+					if (isset($users[$name]))
+					{
+						$users[$name] += intval(substr(strstr($value, ";"), 1));
+						$grade_count[$name] += 1;
+					}
+					else
+					{
+						$users[$name] = intval(substr(strstr($value, ";"), 1));
+						$grade_count[$name] = 1;
+					}
+				}
+			}
+		}
+		ksort($users);
+		foreach($users as $name => $score)
+		{
+			printf("%s:", $name);
+			print($score / $grade_count[$name]);
+			printf("\n");
+		}
+	}
+	if ($argv[1] == "moulinette_variance")
+	{
+		foreach($arr as $value)
+		{
+			if (!(strpos($value, "moulinette")))
+			{
+				$i = 0;
+				while ($value[$i] != ';')
+					$i++;
+				$i++;
+				while ($value[$i] != ';')
+					$i++;
+				$i++;
+				if (is_numeric($value[strpos($value, ';') + 1]))
+				{
+					$name = substr($value, 0, strpos($value, ';'));
+					if (isset($users[$name]))
+					{
+						$users[$name] += intval(substr(strstr($value, ";"), 1));
+						$grade_count[$name] += 1;
+					}
+					else
+					{
+						$users[$name] = intval(substr(strstr($value, ";"), 1));
+						$grade_count[$name] = 1;
+					}
+				}
+			}
+			else
+			{
+				$i = 0;
+				while ($value[$i] != ';')
+					$i++;
+				$i++;
+				while ($value[$i] != ';')
+					$i++;
+				$i++;
+				if (is_numeric($value[strpos($value, ';') + 1]))
+				{
+					$name = substr($value, 0, strpos($value, ';'));
+					if (isset($users_moulinette[$name]))
+					{
+						$users_moulinette[$name] += intval(substr(strstr($value, ";"), 1));
+						$grade_count_moulinette[$name] += 1;
+					}
+					else
+					{
+						$users_moulinette[$name] = intval(substr(strstr($value, ";"), 1));
+						$grade_count_moulinette[$name] = 1;
+					}
+				}
+			}
+		}
+		ksort($users);
+		foreach($users as $name => $score)
+		{
+			printf("%s:", $name);
+			print($score / $grade_count[$name] - $users_moulinette[$name] / $grade_count_moulinette[$name]);
+			printf("\n");
+		}
 	}
 }
-else
-	print("\n");
 ?>
