b74557e592c84c87e2b083889238884888820143
sadawi <hahli144@hotmail.com>
2020-06-25 16:27:31 +0300

    add ex06

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -2,12 +2,18 @@
 
 if ($_SERVER[PHP_AUTH_USER] == "zaz" && $_SERVER[PHP_AUTH_PW] == "Ilovemylittleponey")
 {
-	echo("<html><body>");
-	readfile("../img/42.png");
+	echo "<html><body>\nHello Zaz<br />\n";
+	$path = "../img/42.png";
+	$image = base64_encode(file_get_contents($path));
+	echo "<img src='data:image/png;base64,".$image."'>\n";
+	echo "</body></html>\n";
 }
 else
 {
 	header("WWW-Authenticate: Basic realm=''Member area''");
+	header('HTTP/1.0 401 Unauthorized');
+	echo "<html><body>That area is accessible for members only</body></html>\n";
+	exit;
 }
 
 ?>
