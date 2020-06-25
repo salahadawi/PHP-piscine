2a85f904c27313935518e39a54271328bdc7dcfc
sadawi <hahli144@hotmail.com>
2020-06-25 14:42:03 +0300

    fix ex02 not working if newlines between text

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -6,7 +6,7 @@ if ($argc == 2 && file_exists($argv[1]))
 	$file = file_get_contents($argv[1]);
 	preg_match_all("/(?<=<a)[\s\S]*?(?=\/a>)/", $file, $matches);
 	$str = implode(" ", $matches[0]);
-	preg_match_all("/(\"[\s\S]*?\"|>\s*[A-z].+?\s*<)/", $str, $matches);
+	preg_match_all("/(\"[\s\S]*?\"|>\s*[A-z][\s\S]+?\s*<)/", $str, $matches);
 	$replacement = $matches[0];
 	foreach ($replacement as &$line)
 		$line = strtoupper($line);
